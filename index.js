962c01dd6cbd4417dc28d35a8460afb703c1022f
dygsuheghu <msjcentality666@hotmail.com>
2024-05-08 13:39:02 +0700

    start commit

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const difference = (a, b) => {
-  const s = new Set(b);
-  return a.filter((x) => !s.has(x));
-};
+function merge(intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+  const merged = [];
+  for (const interval of intervals) {
+    if (!merged.length || merged[merged.length - 1][1] < interval[0]) {
+      merged.push(interval);
+    } else {
+      merged[merged.length - 1][1] = Math.max(
+        merged[merged.length - 1][1],
+        interval[1],
+      );
+    }
+  }
+  return merged;
+}
