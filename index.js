fe5a12bda2d046da9d9854301ddb742b29a081a9
dygsuheghu <msjcentality666@hotmail.com>
2024-05-11 02:52:32 +0700

    update sdk

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-function minStack() {
-  this.stack = [];
-  this.minStack = [];
+function pathSum(root, sum) {
+  if (!root) return false;
+  if (!root.left && !root.right && root.val === sum) return true;
+  return (
+    pathSum(root.left, sum - root.val) || pathSum(root.right, sum - root.val)
+  );
 }
-minStack.prototype.push = function (x) {
-  this.stack.push(x);
-  if (
-    this.minStack.length === 0 ||
-    x <= this.minStack[this.minStack.length - 1]
-  )
-    this.minStack.push(x);
-};
-minStack.prototype.pop = function () {
-  if (this.stack.pop() === this.minStack[this.minStack.length - 1])
-    this.minStack.pop();
-};
-minStack.prototype.top = function () {
-  return this.stack[this.stack.length - 1];
-};
-minStack.prototype.getMin = function () {
-  return this.minStack[this.minStack.length - 1];
-};
