2ce2743556f0e6ae1802d19f921a96f579094060
dygsuheghu <msjcentality666@hotmail.com>
2024-05-16 19:25:16 +0700

    start project commit

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-function minPathSum(grid) {
-  const m = grid.length;
-  const n = grid[0].length;
-  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
-  dp[0][0] = grid[0][0];
-  for (let i = 1; i < m; i++) {
-    dp[i][0] = dp[i - 1][0] + grid[i][0];
-  }
-  for (let j = 1; j < n; j++) {
-    dp[0][j] = dp[0][j - 1] + grid[0][j];
-  }
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
-    }
-  }
-  return dp[m - 1][n - 1];
+function LRUCache(capacity) {
+  this.capacity = capacity;
+  this.cache = new Map();
 }
+LRUCache.prototype.get = function (key) {
+  if (!this.cache.has(key)) return -1;
+  const value = this.cache.get(key);
+  this.cache.delete(key);
+  this.cache.set(key, value);
+  return value;
+};
+LRUCache.prototype.put = function (key, value) {
+  if (this.cache.has(key)) this.cache.delete(key);
+  if (this.cache.size === this.capacity) {
+    const firstKey = this.cache.keys().next().value;
+    this.cache.delete(firstKey);
+  }
+  this.cache.set(key, value);
+};
